3686c99b19635f2ef9704970ed70936475d3c538
dhrgodms <dhrgodms123@gmail.com>
2022-12-10 17:25:01 +0900

    221210-2480 : 주사위 세개 게임

---
+++ 221210-2480.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func max(n1 int, n2 int, n3 int) int{
+	var maxNum int
+	if n1>=n2 {
+		if n1>=n3{
+		maxNum = n1
+		} else{
+			maxNum =n3
+		}
+	} else if n2>=n3 {
+		maxNum=n2
+	} else {
+		maxNum=n3
+	}
+	return maxNum
+}
+
+func main()  {
+	var a,b,c int
+	var result int
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+	if a==b&&b==c {
+		result = 10000+ a*1000
+	} else if a==b||b==c {
+		result = 1000 + b*100
+	} else if a==c{
+		result = 1000 + a*100
+	} else{
+		result = max(a,b,c)*100
+	}
+	fmt.Println(result)
+}
