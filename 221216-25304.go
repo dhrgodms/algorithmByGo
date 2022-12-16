b20c4a4e0f3d79f9c8d90238b8f7411228a10818
dhrgodms <dhrgodms123@gmail.com>
2022-12-17 00:33:27 +0900

    221216-25304

---
+++ 221216-25304.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+	var total, n int
+	//fmt.Scanf("%d\n%d", &total, &n) 성공
+	_, err := fmt.Scan(&total, &n) //성공
+	stdin := bufio.NewReader(os.Stdin)
+
+	if err!=nil{
+		fmt.Println(err)
+		stdin.ReadString('\n')
+	} else{
+		var result int
+		for i:=0;i<n;i++ {
+			var a, b int
+			_, err := fmt.Scan(&a, &b)
+			result += a * b
+			if err != nil {
+				fmt.Println(err)
+				stdin.ReadString('\n')
+				return
+			}
+		}
+		if total == result {
+			fmt.Println("Yes")
+		} else {
+			fmt.Println("No")
+		}
+	}
+}
