c556de4af68be4c356478d5c6289afa8fd64aeaf
dhrgodms <dhrgodms123@gmail.com>
2022-12-20 04:16:54 +0900

    221219-2908

---
+++ 221219-2908.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+func main(){
+	var num1, num2 string
+	var num1Result, num2Result int
+
+	stdin := bufio.NewReader(os.Stdin)
+	_, err := fmt.Scan(&num1, &num2)
+
+	if err != nil{
+		fmt.Println(err)
+		stdin.ReadString('\n')
+	}else {
+		for i := 0; i < 3; i++ {
+			num1Result += (int(num1[i])-48) * int(math.Pow(10, float64(i)))
+			num2Result += (int(num2[i])-48) * int(math.Pow(10, float64(i)))
+		}
+
+		if num1Result > num2Result {
+			fmt.Println(num1Result)
+		} else {
+			fmt.Println(num2Result)
+		}
+	}
+}
