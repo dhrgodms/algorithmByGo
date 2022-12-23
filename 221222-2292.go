44c7670128b6cabb9ebe0d7626c4f6f4d9a83c0d
dhrgodms <dhrgodms123@gmail.com>
2022-12-23 09:33:38 +0900

    221222-2292

---
+++ 221222-2292.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+func getIndex(N int) int{
+	n := int(math.Ceil((3+(math.Pow(float64(12*N-3), 0.5)))/6))
+	return n
+}
+
+func main(){
+	var n int
+
+	stdin := bufio.NewReader(os.Stdin)
+	_, err := fmt.Scan(&n)
+
+	if err != nil{
+		fmt.Println(err)
+		stdin.ReadString('\n')
+	} else{
+		fmt.Println(getIndex(n))
+		}
+	}
